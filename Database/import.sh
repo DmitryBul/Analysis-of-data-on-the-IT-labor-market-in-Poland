7c29c501b22dcc042b995401f00ee8f14877918c
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-06-14 05:58:13 +0200

    Changed script format to LF

--- Database/import.sh
+++ Database/import.sh
@@ -3,7 +3,6 @@ echo "Starting import of BSON files..."
 for file in /home/dump/*.bson
 do
     collection_name=$(basename "$file" .bson)
-
     echo "Importing $file into collection $collection_name..."
     mongorestore --db docker-projekt --collection "$collection_name" "$file"
 done
